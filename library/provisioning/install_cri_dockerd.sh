980dfccd48d0839b30d8567406a0bcbe2e24c72d
Elias Reichart <el.reichart@gmail.com>
2022-08-18 14:00:05 +0200

    Change to cri_dockerd

---
+++ library/provisioning/install_cri_dockerd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.2.0/cri-dockerd-v0.2.0-linux-amd64.tar.gz
+
+tar xvf cri-dockerd-v0.2.0-linux-amd64.tar.gz
+
+mv ./cri-dockerd /usr/local/bin/
+
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
